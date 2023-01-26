62460e2842de470adfde63e0cd206b8478584564
ethan <izawaryosuke0101@gmail.com>
2023-01-26 19:05:30 +0900

    Socialコンポーネントを追加

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,15 @@
 import Container from "./container"
 import Logo from "./logo"
 import styles from "styles/footer.module.css"
+import Social from "./social"
+
 export default function Footer(){
   return(
     <footer className={styles.wrapper}>
       <Container>
         <div className={styles.flexContainer}>
         <Logo />
-        [ソーシャル]
+        <Social />
         </div>
       </Container>
     </footer>
