308a9ee9d3247afb36d709a89e950ddf4d7ecf6b
ethan <izawaryosuke0101@gmail.com>
2023-01-26 14:21:25 +0900

    各種コンポーネントをインポート

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from '@/components/header'
+import Hero from '@/components/hero'
+import Footer from '@/components/footer'
+import Layout from '@/components/layout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-    <h1>CUBE</h1>
-    </>
+      <Hero
+      title="CUBE"
+      subtitle="アウトプットしていくサイト"
+      imageOn
+       />
   )
 }
