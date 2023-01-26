66dd0cf5875e714063fbcfffad6711f5d05d3d4c
ethan <izawaryosuke0101@gmail.com>
2023-01-27 00:25:49 +0900

    constants.jsから取得する

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,17 @@
 import Head from "next/head";
+// サイトに関する情報
+import { siteMeta } from 'lib/constants'
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
 
-export default function Meta({pageTitle}){
+export default function Meta({pageTitle, pageDesc}){
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
+  const desc = pageDesc ?? siteDesc
   return(
     <Head>
-      <title>{pageTitle}</title>
-      <meta property="og:title" content={pageTitle} />
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta name="description" content={desc} />
+      <meta property="og:description" content={desc} />
     </Head>
   )
 }
