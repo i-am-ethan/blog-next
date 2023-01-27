17dfe8c8b7110d2d0c4a351b1dc3d2310d96502a
ethan <izawaryosuke0101@gmail.com>
2023-01-27 16:54:25 +0900

    next12で使えないコンポーネントを削除

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import Header from '@/components/header'
 import Hero from '@/components/hero'
 import Footer from '@/components/footer'
 import Layout from '@/components/layout'
 import Container from '@/components/container'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
