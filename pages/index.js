cf05bdd27e70a58750e6e10d18abd6869883839a
ethan <izawaryosuke0101@gmail.com>
2023-01-27 19:23:35 +0900

    topページに記事を追加

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '@/components/header'
-import Hero from '@/components/hero'
-import Footer from '@/components/footer'
-import Layout from '@/components/layout'
-import Container from '@/components/container'
+import { getAllPosts } from 'lib/api'
+import Meta from 'components/meta'
+import Container from 'components/container'
+import Hero from 'components/hero'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
-export default function Home() {
+export default function Home({posts}) {
   return (
     <Container>
-      <Hero
-      title="CUBE"
-      subtitle="アウトプットしていくサイト"
-      imageOn
-       />
+      <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn/>
+      <Posts posts={posts} />
+      <Pagination nextUrl="/blog" nextText="More Posts" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
