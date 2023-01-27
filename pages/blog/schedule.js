59b97d5cabdaf119bc0026886f5b318efa44b71b
ethan <izawaryosuke0101@gmail.com>
2023-01-27 16:54:41 +0900

    記事ページにアイキャッチ画像を追加

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,7 @@
 import { getPostBySlug } from "@/lib/api"
 import Container from "@/components/container"
 import PostHeader from "@/components/post-header"
+import Image from "next/image"
 
 export default function Schedule({
   title, publish, content, eyecatch, categories,
@@ -9,6 +10,18 @@ export default function Schedule({
     <Container>
       <article>
         <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt="アイキャッチ画像"
+            layout="responsive"
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-width: 1152px) 1152px, 100vw"
+            priority
+          />
+        </figure>
       </article>
     </Container>
   )
